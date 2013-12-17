a0318ce82eca77d84420bf00e85ec67cfda8a71b
Joar Wandborg <joar@wandborg.se>
2013-12-17 11:14:45 +0100

    [storage] Proper ABC

--- accounting/storage/__init__.py
+++ accounting/storage/__init__.py
@@ -1,19 +1,35 @@
+from abc import ABCMeta, abstractmethod
 
-class Storage:
+
+class Storage():
     '''
     ABC for accounting storage
     '''
+    __metaclass__ = ABCMeta
+
     def __init__(self, *args, **kw):
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def get_transactions(self, *args, **kw):
-        raise NotImplementedError()
+        raise NotImplementedError
 
+    @abstractmethod
     def get_transaction(self, *args, **kw):
-        raise NotImplementedError()
+        raise NotImplementedError
 
+    @abstractmethod
     def get_account(self, *args, **kw):
-        raise NotImplementedError()
+        raise NotImplementedError
 
+    @abstractmethod
     def get_accounts(self, *args, **kw):
-        raise NotImplementedError()
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_transaction(self, transaction):
+        raise NotImplementedError
+
+    @abstractmethod
+    def reverse_transaction(self, transaction_id):
+        raise NotImplementedError
